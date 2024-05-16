6b85bdff4da8bd17e538ea02dc7210dc85e1336e
meshulamzusha <nauko763@gmail.com>
2024-05-16 10:40:00 +0300

    deleting an unnecessary function

--- elevator-app/src/Building/RenderFloors.tsx
+++ elevator-app/src/Building/RenderFloors.tsx
@@ -23,14 +23,10 @@ export default class RenderFloors extends Component<RenderFloorsProps, RenderFlo
     private createFloors(): void {
         for (let i = 0; i < config.numberOfFloors; i++) {
             const floorNumber = config.numberOfFloors - i - 1;
-            const newFloor = new Floor({ floorNumber: floorNumber, onClick: this.handleFloorClick });
+            const newFloor = new Floor({ floorNumber: floorNumber, onClick: this.props.onFloorButtonClick });
             this.state.floors.push(newFloor);
         }
     }
-
-    private handleFloorClick = (floorNumber: number): void => {
-        this.props.onFloorButtonClick(floorNumber);
-    };
 
     render() {
         return (
